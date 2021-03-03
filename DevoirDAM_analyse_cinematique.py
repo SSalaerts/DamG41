98de49bbcb4d9ec6611cec5922fa5db08109547a
Francis <fransuiss1016@gmail.com>
2021-03-03 18:17:53 +0100

    Début d'écriture du code pour le devoir
    j'ai écrit les grandeurs géométriques ainsi que les fonctions calculant le volume, la chaleur et leurs dérivées par rapport à theta

---
+++ DevoirDAM_analyse_cinematique.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+"""
+Analyse cinématique et dynamique en vue de dimensionner la section d'une bielle.
+
+Le moteur étudié est un moteur Diesel à 4 temps
+largement basé sur le moteur trouvé au lien https://powersuite.cummins.com/PS5/PS5Content/SiteContent/en/Binary_Asset/pdf/KentData/DataSheets/FR30233.pdf
+
+La longueur de la bielle n'étant pas référencée, nous avons décidé d'un ratio beta arbitraire de 3.
+"""
+
+# Grandeurs géométriques
+
+D = 0.095          # diamètre du piston en mètres
+C = 0.115          # course du piston en mètres
+Vc = (3.3E-3)/4    # cylindrée d'un piston
+R = C/2            # longueur de la manivelle obtenue grâce à la longueur de la course
+tau = 18           # taux de compression
+beta = 3           # ratio entre la longueur de la bielle et la manivelle
+L = R*beta         # longueur de la bielle que l'on cherche a dimensionner
+
+m_piston = 0.25 # masse du piston en kg
+m_bielle = 0.35 # masse de la bielle
+
+Qtot = 0 # TODO
+
+# Fonctions calculant le volume, la chaleur ainsi que leur dérivée par rapport à theta
+
+V_theta = lambda theta : (Vc/2)*(1 - np.cos(theta) + beta - np.sqrt(theta*theta - np.sin(theta)**2)) + Vc/(tau - 1)
+dVdtheta = lambda theta : (Vc/2)*(np.sin(theta) + (np.sin(theta)*np.cos(theta))/np.sqrt(beta*beta - np.sin(theta)**2))
+
+Q_theta = lambda theta, theta_d, theta_comb : (Qtot/2)*(1 - np.cos(np.pi*((theta - theta_d)/theta_comb)))
+dQdtheta = lambda theta, theta_d, theta_comb : (Qtot*np.pi/2*theta_comb)*np.sin(np.pi*(theta-theta_d)/theta_comb)
+
--- duCode.py
+++
@@ -1,4 +0,0 @@
-# just some random shit
-# salut les gens
-
-print("ma grosse bi... bielle")
