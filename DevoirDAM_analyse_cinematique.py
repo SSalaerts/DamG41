147496380c460e1f51f6a00a894777b66dcedc14
Francis <fransuiss1016@gmail.com>
2021-03-12 21:20:24 +0100

    PTDR go debug et c'est bon en fait
    j'ai rejouté les fonctions restantes mais il reste à débugger ce qui va pas je pense

--- DevoirDAM_analyse_cinematique.py
+++ DevoirDAM_analyse_cinematique.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 """
 Analyse cinématique et dynamique en vue de dimensionner la section d'une bielle.
 
@@ -18,63 +18,111 @@ R = C/2            # longueur de la manivelle obtenue grâce à la longueur de l
 beta = 3           # ratio entre la longueur de la bielle et la manivelle
 L = R*beta         # longueur de la bielle que l'on cherche à dimensionner
 mpiston = 0.25     # masse du piston en kg
-mbielle = 0.35     # masse de la bielle
-Q = 165000         # valeur chaleur emise par fuel par kg de melange admis (Diesel) en J
-
-Vc = (3.3e-3)/4    # cylindrée d'un piston
-
- #énergie par kg pour le Diesel * la masse trouvée grâce au premier principe : pV = m*R#*T
-                                          # (pression d'admission de 2.5 bar car presence de turbo, temperature du gaz de 30°C = 303.15 K
-                                          # et R# = 287.1 pour de l'air (néglige le carburant) TODO vérifier que les approx sont légitimes
-
-gamma = 1.3
+mbielle = 0.35     # masse de la bielle en kg
+Q = 1650e3         # valeur chaleur emise par fuel par kg de melange admis (Diesel) en J
+Vc = 3.3e-3/4    # cylindrée d'un piston
 
 # Fonctions calculant le volume, la chaleur ainsi que leur dérivée par rapport à theta
 
-V_theta = lambda theta : (Vc/2)*(1 - np.cos(theta) + beta - np.sqrt(theta*theta - np.sin(theta)**2)) + Vc/(tau - 1)
+V_theta = lambda theta : (Vc/2)*(1 - np.cos(theta) + beta - np.sqrt(beta*beta - np.sin(theta)**2)) + Vc/(tau - 1)
 dVdtheta = lambda theta : (Vc/2)*(np.sin(theta) + (np.sin(theta)*np.cos(theta))/np.sqrt(beta*beta - np.sin(theta)**2))
 
 Q_theta = lambda Qtot, theta, thetaC, deltaThetaC : (Qtot / 2) * (1 - np.cos(np.pi * ((theta - thetaC) / deltaThetaC)))
 dQdtheta = lambda Qtot, theta, thetaC, deltaThetaC : (Qtot*np.pi/2*deltaThetaC)*np.sin(np.pi*(theta-thetaC)/deltaThetaC)
 
+
 # Fonction calculant la dérivée par rapport à theta de la pression
-
-dPdtheta = lambda p, theta, thetaC, deltaThetaC : -gamma*p*dVdtheta(theta)/V_theta(theta) + (gamma - 1)*dQdtheta(theta, thetaC, deltaThetaC)/V_theta(theta)
-
-# Euler explicite en fait
-# A vérifier si c'est bien comme implémentation
-
-def eulerExplicite(thetaC, deltaThetaC):
-
-    P = np.zeros(360)
-    P[0] = 2.5e5
-
-    for theta in range(360):
-        P[theta+1] = P[theta] + dPdtheta(P[theta], theta, thetaC, deltaThetaC)
-        if(thetaC == deltaThetaC): # TODO les conditions sont pas bonnes / maybe yen a pas besoin dépendant de ma fonction
-            pass
-        else:
-            pass
-
-    return P
-
-
 def myfunc(rpm, s, theta, thetaC, deltaThetaC):
 
-    Qtot = Q*(s*(1e5)*Vc)/(287.1*303.15)
+    DegtoRad = 2*np.pi/360
+    thetaRadian = theta*DegtoRad
+    thetaCRadian = thetaC*DegtoRad
+    deltaThetaCRadian = deltaThetaC*DegtoRad
 
+    h = np.abs(thetaRadian[0] - thetaRadian[1])
     omega = rpm*2*np.pi/60
+    p_admission = s*1e5
+    Qtot = Q * (p_admission * Vc) / (287.1 * 303.15)
+    gamma = 1.3
 
-    V_output = V_theta(theta)
-    Q_output = Q_theta(Qtot, theta, thetaC, deltaThetaC)
+    size = theta.size
+    p_output = np.zeros(size)
+    F_pied_output = np.zeros(size)
+    F_tete_output = np.zeros(size)
+    Fcrit = 0
 
-    dV = dVdtheta(theta)
-    dQ = dQdtheta(Qtot, theta, thetaC, deltaThetaC)
+    V_output = V_theta(thetaRadian)
+    Q_output = Q_theta(Qtot, thetaRadian, thetaCRadian, deltaThetaCRadian)
 
-    p_output = 0
-    F_pied_output = 0
-    F_tete_output = 0
-    t = 0
+    dV = dVdtheta(thetaRadian)
+    dQ = dQdtheta(Qtot, thetaRadian, thetaCRadian, deltaThetaCRadian)
+
+    dPdtheta = lambda p, i: -gamma * p * dV[i] / V_output[i] + (
+                gamma - 1) * dQ[i] / V_output[i]
+
+    """Calcul de p par Euler explicite"""
+    p_output[0] = p_admission
+    for i in range(size - 1):
+        p_output[i + 1] = p_output[i] + h*dPdtheta(p_output[i], i)
+
+    """Calcul de F_pied_output et F_tete_output"""
+    for i in range(size):
+        pression = (np.pi*D*D/4)*p_output[i]
+        acceleration = R*omega*omega*np.cos(thetaRadian[i])
+        F_pied_output[i] = pression - mpiston*acceleration
+        F_tete_output[i] = -pression + (mpiston + mbielle)*acceleration
+
+    """Calcul de la force critique"""
+
+    for i in range(size):
+        F_pied = F_pied_output[i]
+        F_tete = F_tete_output[i]
+
+        if(F_pied >= 0 and F_tete <= 0):
+            F_compression = min(F_pied, -F_tete)
+            if(Fcrit > F_compression):
+                Fcrit = F_compression
+
+    """Calcul de t"""
+    sigma = 450e6   # résistance de compression 450 MPa
+    E = 200e9       # module d'élasticité 200 GPa
+    Kx = 1          # facteur de correction dans le plan du mouvement
+
+    coeffEuler = (419*np.pi*np.pi*E)/(12*Kx*Kx*L*L)
+    a = 11*coeffEuler
+    b = coeffEuler
+    c = 11*Fcrit
+    racineDelta = np.sqrt(b*b - 4*a*c)
+
+    t1 = np.sqrt((b - racineDelta)/2*a)
+    t2 = np.sqrt((b + racineDelta)/2*a)
+
+    t = max(t1, t2)
 
     return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)
 
+
+rpm = 2500
+s = 1
+theta = np.arange(-180, 181)
+thetaC = 40
+deltaThetaC = 70
+
+V_output, Q_output, F_pied_output, F_tete_output, p_output, t = myfunc(rpm, s, theta, thetaC, deltaThetaC)
+
+plt.figure()
+plt.plot(theta, V_output, label="Volume par rapport a theta")
+plt.legend()
+
+plt.figure()
+plt.plot(theta, Q_output, label="Chaleur par rapport a theta")
+plt.legend()
+plt.figure()
+plt.plot(theta, F_pied_output, label="Force sur le pied de vilebrequin")
+plt.plot(theta, F_tete_output, label="Force sur la tete du vilebrequin")
+plt.legend()
+plt.figure()
+plt.plot(theta, p_output, label="Pression en bar par rapport a theta")
+print("la section t du vilebrequin vaut: ", t)
+plt.legend()
+plt.show()
