c98907360595771de257a8d62f4b22613ffd4de9
Francis <fransuiss1016@gmail.com>
2021-03-12 22:43:59 +0100

    OKay là on est pas mal
    j'ai plus ou moins réparé la pression, je pense elle est pas mal du tout mais ya toujours soucis de section

--- DevoirDAM_analyse_cinematique.py
+++ DevoirDAM_analyse_cinematique.py
@@ -34,18 +34,18 @@ dQdtheta = lambda Qtot, theta, thetaC, deltaThetaC : (Qtot*np.pi/2*deltaThetaC)*
 # Fonction calculant la dérivée par rapport à theta de la pression
 def myfunc(rpm, s, theta, thetaC, deltaThetaC):
 
-    DegtoRad = 2*np.pi/360
+    DegtoRad = 2*np.pi/360 # qui est aussi l'écart entre deux éléments du tableau thetaRadian
     thetaRadian = theta*DegtoRad
     thetaCRadian = thetaC*DegtoRad
     deltaThetaCRadian = deltaThetaC*DegtoRad
 
-    h = np.abs(thetaRadian[0] - thetaRadian[1])
+    size = theta.size
+
     omega = rpm*2*np.pi/60
     p_admission = s*1e5
     Qtot = Q * (p_admission * Vc) / (287.1 * 303.15)
     gamma = 1.3
 
-    size = theta.size
     p_output = np.zeros(size)
     F_pied_output = np.zeros(size)
     F_tete_output = np.zeros(size)
@@ -56,14 +56,15 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
 
     dV = dVdtheta(thetaRadian)
     dQ = dQdtheta(Qtot, thetaRadian, thetaCRadian, deltaThetaCRadian)
+    dQ[dQ < 180 + thetaC] = 0
+    dQ[dQ > 180 + thetaC + deltaThetaC] = 0
 
-    dPdtheta = lambda p, i: -gamma * p * dV[i] / V_output[i] + (
-                gamma - 1) * dQ[i] / V_output[i]
+    dPdtheta = lambda p, i: (-gamma * p * dV[i] + (gamma - 1) * dQ[i])/V_output[i]
 
     """Calcul de p par Euler explicite"""
     p_output[0] = p_admission
     for i in range(size - 1):
-        p_output[i + 1] = p_output[i] + h*dPdtheta(p_output[i], i)
+        p_output[i + 1] = p_output[i] + DegtoRad*dPdtheta(p_output[i], i)
 
     """Calcul de F_pied_output et F_tete_output"""
     for i in range(size):
@@ -83,8 +84,9 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
             if(Fcrit < F_compression):
                 Fcrit = F_compression
 
+    print("Force critique {} [N], ps je suis dans myfunc()".format(Fcrit))
 
-    """Calcul de t """ #TODO ça pue encore du cul, on a 2.67e17 m de section, c'est beaucoup trop, aussi j'ai pas fait dans le sens perpendiculaire au mouvemement
+    """Calcul de t """ #TODO ça pue encore du cul, on a 6e-7 m de section, c'est beaucoup trop peu, aussi j'ai pas fait dans le sens perpendiculaire au mouvemement
     sigma = 450e6   # résistance de compression 450 MPa
     E = 200e9       # module d'élasticité 200 GPa
     Kx = 1          # facteur de correction dans le plan du mouvement
@@ -93,18 +95,15 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     a = coeffEuler/Fcrit
     b = coeffEuler/11*sigma
 
-    racineDelta = np.sqrt(b*b + 4*a)
+    roots = np.roots([-1, 0, -b, 0, a])
 
-    t1 = np.sqrt((b - racineDelta)/2*a)
-    t2 = np.sqrt((b + racineDelta)/2*a)
-
-    t = max(t1, t2)
+    t = np.real((np.amax(roots)))
 
     return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)
 
 
 rpm = 3000
-s = 1
+s = 2.5
 theta = np.arange(-180, 181)
 thetaC = 40
 deltaThetaC = 70
@@ -113,25 +112,25 @@ V_output, Q_output, F_pied_output, F_tete_output, p_output, t = myfunc(rpm, s, t
 
 
 def beauPlot():
-    plt.figure()
-    plt.plot(theta, V_output)
-    plt.title("Volume par rapport a theta en [m^3]")
+    # plt.figure()
+    # plt.plot(theta, V_output)
+    # plt.title("Volume par rapport a theta en [m^3]")
 
-    plt.figure()
-    plt.plot(theta, Q_output)
-    plt.title("Chaleur par rapport a theta en [J]")
+    # plt.figure()
+    # plt.plot(theta, Q_output)
+    # plt.title("Chaleur par rapport a theta en [J]")
 
     plt.figure()
     plt.plot(theta, F_pied_output, label="F_pied")
     plt.plot(theta, F_tete_output, label="F_tete")
-    plt.title("Force sur le pied et la tete de bielle en [J]")
+    plt.title("Force sur le pied et la tete de bielle en [N]")
     plt.legend()
 
     plt.figure()
-    plt.plot(theta, p_output)
-    plt.title("Pression en bar par rapport a theta en [Pa]")
+    plt.plot(theta, p_output/1e5)
+    plt.title("Pression en bar par rapport a theta en [bar]")
 
-    print("la section t de la bielle vaut: {} en [m]".format(t))
+    print("la section t de la bielle vaut: {} [m]".format(t))
 
     plt.show()
 
